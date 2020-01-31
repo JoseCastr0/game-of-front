efa39b0910389c52281f2f0d3ca6f0441195bda2
Jose Castro <josecastropuertollano@gmail.com>
2020-01-31 22:02:08 +0100

    # styled details + scrolbar

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import './details.scss';
 import LogoHouse from '../logoHouse/LogoHouse';
+import SimpleBarReact from "simplebar-react";
+import 'simplebar/dist/simplebar.min.css';
 
 function CharacterDetail() {
     let { slug } = useParams();
@@ -17,87 +19,94 @@ function CharacterDetail() {
 
 
     return (
-        <main className='c-details container-fluid'>
-            <div className="row c-details__row">
-                <div className="col-12">
+        <main className='c-details'>
+            <div className='container-fluid'>
+                <div className='c-details__title-row'>
+                    {/* <div className=""> */}
                     <img
-                        className='c-details__main-img'
+                        className='c-details__main-img '
                         src={details.image}
                         alt={details.name}
                     />
                     <h2>{details.name}</h2>
+                    {/* </div> */}
+                </div>
+                <div className='c-details__details-row'>
+                    <div className="row">
+                        <div className='c-details__details-row'>
+                            <div className='col-2'>
+                            <div className='c-details__col'>
+                                <h3 className='c-details__col-title'>{t('houses')}</h3>
+                                <LogoHouse
+                                    characterName={details.house}
+                                />
+                            </div>
+                            </div>
+                            <div className='col-2'>
+                                <div className='c-details__col'>
+                                    <h3 className='c-details__col-title'>{t('allegiances')}</h3>
+                                    <ul className='c-details__items-list'>
+                                        {
+                                            details.allegiances && details.allegiances.map(el => {
+                                                return <li className='c-details__item' key={el}>{el}</li>
+                                            })
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='col-2'>
+                                <div className='c-details__col'>
+                                    <h3 className='c-details__col-title'>{t('appearances')}</h3>
+                                    <ul className='c-details__items-list'>
+                                        <SimpleBarReact className='o-simple-bar'>
+                                            {
+                                                details.appearances && details.appearances.map(el => {
+                                                    return <li className='c-details__item' key={el}>{el}</li>
+                                                })
+                                            }
+                                        </SimpleBarReact>
+                                    </ul>
+
+                                </div>
+                            </div>
+                            <div className='col-2'>
+                                <div className='c-details__col'>
+                                    <h3 className='c-details__col-title'>{t('father')}</h3>
+                                    <ul className='c-details__items-list'>
+                                        {
+                                            <li className='c-details__item'>{details.father}</li>
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='col-2'>
+                                <div className='c-details__col'>
+                                    <h3 className='c-details__col-title'>{t('siblings')}</h3>
+                                    <ul className='c-details__items-list'>
+                                        {
+                                            details.siblings && details.siblings.map(el => {
+                                                return <li className='c-details__item' key={el}>{el}</li>
+                                            })
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='col-2'>
+                                <div className='c-details__col'>
+                                    <h3 className='c-details__col-title'>{t('titles')}</h3>
+                                    <ul className='c-details__items-list'>
+                                        {
+                                            details.titles && details.titles.map(el => {
+                                                return <li className='c-details__item' key={el}>{el}</li>
+                                            })
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="row c-details__row">
-                <div className="col-2 c-details__col">
-                    <h3 className='c-details__col-title'>{t('houses')}</h3>
-                    <LogoHouse
-                        characterName={details.house}
-                    />
-                </div>
-                <div className="col-2 c-details__col">
-                    <h3 className='c-details__col-title'>{t('allegiances')}</h3>
-                    <ul className='c-details__items-list'>
-                        {
-                            details.allegiances && details.allegiances.map(el => {
-                                return <li className='c-details__item' key={el}>{el}</li>
-                            })
-                        }
-                    </ul>
-                </div>
-                <div className="col-2 c-details__col">
-                    <h3 className='c-details__col-title'>{t('appearances')}</h3>
-                    <ul className='c-details__items-list'>
-                        {
-                            details.appearances && details.appearances.map(el => {
-                                return <li className='c-details__item' key={el}>{el}</li>
-                            })
-                        }
-                    </ul>
-                </div>
-                <div className="col-2 c-details__col">
-                    <h3 className='c-details__col-title'>{t('father')}</h3>
-                    <ul className='c-details__items-list'>
-                        {                           
-                            <li className='c-details__item'>{details.father}</li>
-                        }
-                    </ul>
-                </div>
-                <div className="col-2 c-details__col">
-                    <h3 className='c-details__col-title'>{t('siblings')}</h3>
-                    <ul className='c-details__items-list'>
-                        {
-                            details.siblings && details.siblings.map(el => {
-                                return <li className='c-details__item' key={el}>{el}</li>
-                            })
-                        }
-                    </ul>
-                </div>
-                <div className="col-2 c-details__col">
-                    <h3 className='c-details__col-title'>{t('titles')}</h3>
-                    <ul className='c-details__items-list'>
-                        {
-                            details.titles && details.titles.map(el => {
-                                return <li className='c-details__item' key={el}>{el}</li>
-                            })
-                        }
-                    </ul>
-                </div>
-            </div>
-
-
-            {/* <div className=''>
-
-                <section className='c-details__info-wrapper'>
-                    <div className='c-details__info-col'>
-
-                    </div>
-                    <div className='c-details__info-col'>
-
-                    </div>
-                </section>
-            </div> */}
-
         </main>
     );
 }
