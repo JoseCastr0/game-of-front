4e067456628333e0ad157ce04f4ed4eeb67a78d5
Jose Castro <josecastropuertollano@gmail.com>
2020-02-05 16:31:30 +0100

    + ordered cronology

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,58 +1,135 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Component } from 'react';
 
-const Timeline = () => {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    fetch('https://api.got.show/api/show/characters/')
-      .then(res => res.json())
-      .then(setCharacters)
-  }, []); 
-
-  function orderCharacters() {
-    //setCharacters(filterAge.sort((a, b) => sortByAge(a, b, DSC)))
+class Timeline extends Component {
+  constructor() {
+    super()
+    this.state = {
+      characters: [],
+      isDesc: false
+    }
   }
 
-  const ASC = 'ascending';
-  const DSC = 'descending';
+  componentDidMount() {
+    axios.get('https://api.got.show/api/show/characters/')
+      .then(res => {
+        console.log(res);
+        let filteredData = res.data.filter(char => {
+          return char.age !== null
+        }).filter(el => {
+          return el.age.age !== undefined
+        })
 
+        this.setState({ characters: filteredData.sort((a, b) => a.age.age - b.age.age)});
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  orderCharacters() {
+    this.setState({ characters: this.state.characters.sort((a, b) => this.sortByAge(a, b)) });
+  }
   
-
-  const filteredData = characters.filter(char => {
-    if(char.age !== null) {
-      return char;
-    }
-  })
-  const filterAge = filteredData.filter(el => {
-    console.log('el: ', el.age.age)
-    return el.age.age !== undefined
-  })
-
-  function sortByAge(a, b, order = ASC) {
+  sortByAge(a, b) {
+    const { isDesc } = this.state;
     const diff = a.age.age - b.age.age;
 
-    if (order === ASC) {
-        return diff;
+    if (isDesc) {
+      this.setState({ isDesc: false });
+      return diff;
+    } else {
+      this.setState({ isDesc: true });
+      return -1 * diff;
     }
-
-    return -1 * diff;
   }
-  
-  setCharacters(filterAge);
-  
-  console.log('filtered characters: ', filterAge);
-  return (
-    <div>
-      <button onClick={orderCharacters()}>Order</button>
-      {
-        filterAge.map(character => {
-          return (
-            <div>Name: {character.name} - edad: {character.age.age}</div>
+
+  //setCharacters(filterAge);
+  render() {
+    const { characters } = this.state;
+    return (
+      <div>
+        <button onClick={this.orderCharacters.bind(this)}>Order</button>
+        {
+          characters.map(character => {
+            return (
+              <div>Name: {character.name} - edad: {character.age.age ? character.age.age : ''}</div>
             )
           })
         }
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default Timeline;
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// const Timeline = () => {
+//   const [characters, setCharacters] = useState([]);
+
+//   // useEffect(() => {
+//   //   fetch('https://api.got.show/api/show/characters/')
+//   //     .then(res => res.json())
+//   //     .then(setCharacters)
+//   // }, []); 
+
+//   useEffect(() => {
+//     axios.get('https://api.got.show/api/show/characters/')
+//       .then(res => {
+//         console.log(res);
+//         let filteredData = res.data.filter(char => {
+//           return char.age !== null
+//         }).filter(el => {
+//           return el.age.age !== undefined
+//         })
+//         setCharacters(filteredData);
+//         console.log('filteredData', filteredData)
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       })
+//   }, [characters]); 
+
+//   function orderCharacters() {
+//     console.log('clik');    
+//     const orderedChars = characters.sort((a, b) => sortByAge(a, b, DSC));
+//     setCharacters(orderedChars);
+//     console.log('CHARACTERS: ', characters);    
+//   }
+
+//   const ASC = 'ascending';
+//   const DSC = 'descending';
+
+
+//   function sortByAge(a, b, order = ASC) {
+//     const diff = a.age.age - b.age.age;
+
+//     if (order === ASC) {
+//         return diff;
+//     }
+
+//     return -1 * diff;
+//   }
+
+//   //setCharacters(filterAge);
+
+//   return (
+//     <div>
+//       {/* <button onClick={(orderCharacters)}>Order</button> */}
+//       <button onClick={() => setCharacters(characters.sort((a, b) => sortByAge(a, b, DSC)))}>Order</button>
+//         {
+//           characters.map(character => {
+//             return (
+//                 <div>Name: {character.name} - edad: {character.age.age ? character.age.age : '' }</div>
+//               )
+//             })
+//         }
+//     </div>
+//   );
+// }
+
+// export default Timeline;
