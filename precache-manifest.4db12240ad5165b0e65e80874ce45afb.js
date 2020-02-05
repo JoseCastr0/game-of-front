ba34fd7f5bef25e4461c8d445bd35940f61f72e9
Jose Castro <josecastropuertollano@gmail.com>
2020-02-05 20:45:01 +0100

    Updates

---
+++ precache-manifest.4db12240ad5165b0e65e80874ce45afb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e6cd91bb35e793371605861eae529a",
+    "url": "/game-of-front/index.html"
+  },
+  {
+    "revision": "daffc3176411bc9047c6",
+    "url": "/game-of-front/static/css/2.daba7128.chunk.css"
+  },
+  {
+    "revision": "1c23a3fdb2409b98d394",
+    "url": "/game-of-front/static/css/main.4816f6bd.chunk.css"
+  },
+  {
+    "revision": "daffc3176411bc9047c6",
+    "url": "/game-of-front/static/js/2.63223aa1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/game-of-front/static/js/2.63223aa1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c23a3fdb2409b98d394",
+    "url": "/game-of-front/static/js/main.6bbe4959.chunk.js"
+  },
+  {
+    "revision": "a72bc3abd22e6ced919c",
+    "url": "/game-of-front/static/js/runtime-main.695fed90.js"
+  },
+  {
+    "revision": "3af178ecd64adfa098e7456e5830e5f8",
+    "url": "/game-of-front/static/media/Game_of_Thrones.3af178ec.ttf"
+  },
+  {
+    "revision": "e631d2735799aa943d93d301abf423d2",
+    "url": "/game-of-front/static/media/Lato-Black.e631d273.ttf"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/game-of-front/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/game-of-front/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/game-of-front/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "3aa4d3febbeb5bce6613800b6615377e",
+    "url": "/game-of-front/static/media/spain_flag.3aa4d3fe.svg"
+  },
+  {
+    "revision": "46457879840f6939d02c6e0f0f859598",
+    "url": "/game-of-front/static/media/throne-back.46457879.png"
+  },
+  {
+    "revision": "4bb7a074580d2c729816286abd336394",
+    "url": "/game-of-front/static/media/uk_flag.4bb7a074.svg"
+  }
+]);
